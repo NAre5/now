f3fe962b37a43ed7c90be78c616b3aa075322d05
michaelmichaelshvili <michaelm748@gmail.com>
2018-03-14 20:26:07 +0200

    addCandidates

--- ex1.py
+++ ex1.py
@@ -1,14 +1,32 @@
-import subprocess
 
 def printid ():
-    return "208278861_318949443"
+    return "318949443_208278861"
+
 
 def addCandidate(fname, lname, party, filetype):
+    if filetype == 't':
+        file = open("Candidates.txt", 'a+')
+        cid=0
+        context = reversed(file.readlines())
+        for line in context:
+            if party == line.strip().split(',')[3]:
+                cid = line.strip.split(',')[0]+1
+                break
+        file.write("%i,%s,%s,%s\n" % (cid,fname,lname,party))
+    else:
+        file = open("cab.bin", 'ab+')
+        cid = 0
+        context = reversed(file.readlines())
+        for line in context:
+            if party == line.strip().split(',')[3]:
+                cid = line.strip.split(',')[0] + 1
+                break
+        file.write("%i,%s,%s,%s"% (cid, fname, lname, party))
     return "fail"
 
 
 def deleteCandidate(cid,filetype):
-    subprocess.call(['sed','-i','/.*204.*/d','can.txt'])
+    #subprocess.call(['sed','-i','/.*204.*/d','can.txt'])
     return "fail"
 
 def addState(sid, sname,filetype):
@@ -28,7 +46,7 @@ def deletePoll(pid,filetype):
 
 ## recordId: id of the record that should be updated, fieldname: field to update, newValue: new value in field name.
 def updateFile(recordId,fieldname,newValue,file,filetype):
-    return "fail"
+	return "fail"
 
 
 def sort(filename,filetype,fieldname):
@@ -67,5 +85,12 @@ print(names)
 File.close()
 '''
 
+'''
 deleteCandidate(0,'')
-
+for line in reversed(open("Candidates.bin").readlines()):
+    print line.rstrip()
+con = reversed(open("Candidates.txt").readlines())
+for line in con:
+    print line.strip().split(',')[3]
+'''
+addCandidate('michael','shvili','Republican','b')
