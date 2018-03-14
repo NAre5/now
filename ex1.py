d177346dec916f3e5aa7abc5800d2cabc7824250
NAre5 <sherlord5@gmail.com>
2018-03-14 21:23:48 +0200

    add eran bla bla

--- ex1.py
+++ ex1.py
@@ -1,30 +1,55 @@
 import subprocess
 
+def File_Type(c):
+    dict = {'t':'txt','b':'bin'}
+    if dict.has_key(c):
+        return dict[c]
+    raise NameError('should be "t" or "b"')
+
+
 def printid ():
     return "208278861_318949443"
 
 def addCandidate(fname, lname, party, filetype):
     return "fail"
 
+def delete_from(fileName,content):
+    File = open(fileName, "r+")
+    Lines = File.readlines()
+    File.seek(0)
+    for line in Lines:
+        splitted = line.strip().split(',')
+        print(splitted[0])
+        if str(content) == splitted[0]:
+            continue
+        File.write(line)
+    File.truncate()
+    File.close()
+    return "success"
 
 def deleteCandidate(cid,filetype):
-    #subprocess.call(['sed','-i','/.*204.*/d','can.txt'])
-    subprocess.call(['ls'])
-    return "fail"
+    return delete_from('Candidates.'+File_Type(filetype),cid)
 
 def addState(sid, sname,filetype):
     return "fail"
 
 
 def deleteState(sid,filetype):
-    return "fail"
+    return delete_from('States'+File_Type(filetype),sid)
 
 def addPoll(pid,party,state,res,filetype):
     return "fail"
 
 
 def deletePoll(pid,filetype):
-    return "fail"
+    File = open('States'+File_Type(filetype), 'r+')
+    arr = [[]]
+    for line in File:
+        arr.append(line.strip().split(','))
+    arr.remove(arr[0])
+    names = [li[1] for li in (ar for ar in arr)]
+    File.close()
+    return delete_from('Polls'+File_Type(filetype),pid)
 
 
 ## recordId: id of the record that should be updated, fieldname: field to update, newValue: new value in field name.
@@ -53,20 +78,13 @@ def returnLine(filename,filetype,linenumber):
     return ""
 
 
+deleteCandidate(204,'t')
 '''
-
 File = open('Candidates.txt', 'r+')
 arr=[[]]
 for line in File:
      arr.append(line.strip().split(','))
 arr.remove(arr[0])
-
-
-print(arr[1][1])
 names = [li[1] for li in (ar for ar in arr)]
-print(names)
 File.close()
 '''
-
-subprocess.call('dir', shell=True)
-subprocess.check_call(["ls", "-l"],shell=True)
