2669fc417cc237c3dd58d3291da8de762bf88df6
michaelmichaelshvili <michaelm748@gmail.com>
2018-03-14 19:36:18 +0200

    spaces

--- ex1.py
+++ ex1.py
@@ -4,35 +4,48 @@ def printid ():
 
 def addCandidate(fname, lname, party, filetype):
     return "fail"
+
+
 def deleteCandidate(cid,filetype):
     return "fail"
 
 def addState(sid, sname,filetype):
     return "fail"
+
+
 def deleteState(sid,filetype):
     return "fail"
 
 def addPoll(pid,party,state,res,filetype):
     return "fail"
+
+
 def deletePoll(pid,filetype):
     return "fail"
+
 
 ## recordId: id of the record that should be updated, fieldname: field to update, newValue: new value in field name.
 def updateFile(recordId,fieldname,newValue,file,filetype):
 	return "fail"
 
+
 def sort(filename,filetype,fieldname):
     return "fail"
+
 
 def selectPollsFromStateAndParty(state,party,filetype):
     l = []
     return l
+
+
 def selectPollsForParty(party,filetype):
     l = []
     return l
 
 def find(filename,filetype,findme):
     return -1
+
+
 def returnLine(filename,filetype,linenumber):
     return ""
 
