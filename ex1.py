1f467d82ea0372cad779bb8983a6aae76bf6db27
michaelmichaelshvili <michaelm748@gmail.com>
2018-03-19 00:19:56 +0200

    add poll done

--- ex1.py
+++ ex1.py
@@ -1,16 +1,10 @@
-
-def File_Type(c):
-    dict = {'t':'txt','b':'bin'}
-    if dict.has_key(c):
-        return dict[c]
-    raise NameError('should be "t" or "b"')
-
-
 def printid ():
     return "318949443_208278861"
 
 
 def addCandidate(fname, lname, party, filetype):
+    if party != "Democratic" or party != "Republican":
+        return "fail"
     file = None
     if filetype == 't':
         file = open("can.txt", 'a+')
@@ -41,13 +35,12 @@ def addState(sid, sname,filetype):
     else:
         file = open('copystatesbin.bin', 'ab+')
     context = file.readlines()
-    existstate = False
     for line in context:
-        li = line.strip().split(',')[1]
-        if sid == li:
+        line = line.strip().split(',')[1]
+        if sid == line:
             print "fail"
             file.close()
-            return "fail"
+            return "fail state already exist"
     file.write("%s,%s\n" % (sname, sid))
     file.close()
     return "fail"
@@ -57,26 +50,86 @@ def deleteState(sid,filetype):
     return "fail"
 
 
-#check if the state is exists
-def addPoll(pid,party,state,res,filetype):
-    file = None
-    if filetype == 't':
-        file = open('copystates.txt', 'a+')
-    else:
-        file = open('copystates.bin', 'ab+')
-    context = file.readlines()
-    existstate = False
-    for line in context:
-        li = line.strip().split(',')[1]
-        if state == li:
-            existstate = True
-            break
-    file.close()
-    if not existstate:
-        print "fail"
+def addPoll(pid, party, state, res, filetype):
+    if party != "Democratic" and party != "Republican":
+        print "fail party in poll"
+        return "fail"
+
+    # precent not above 100
+    split = res.split('%')
+    split = split[:-1]
+    sumPrecent = 0
+    for i in split:
+        sumPrecent = sumPrecent + int(float(i.split(' ')[-1]))
+    if sumPrecent > 100:
+        print "fail precents above 100"
         return "fail"
 
 
+    file = None
+
+    #check candidates
+    if filetype == 't':
+        file = open('can.txt', 'r+')
+    else:
+        file = open('can.bin', 'rb+')
+    context = file.readlines()
+    file.close()
+
+    split = res.replace('%-', '%')
+    split = split.split('%')
+    split = split[:-1]
+    i = 0
+    for part in split:
+        part = str(part)
+        part = part.split(' ')[:2]
+        split[i] = part[0] + "," + part[1]+","+party
+        candidFound = False
+        for line in context:
+            line = line.split(',')[1:]
+            line = line[0]+","+line[1]+","+line[2]
+            line = line.strip()
+            if split[i] == line:
+                candidFound = True
+                break
+        if not candidFound:
+            print "fail candidate did not found"
+            return "fail"
+        i = i + 1
+
+    # exist state
+    if filetype == 't':
+        file = open('copystates.txt', 'r+')
+    else:
+        file = open('copystates.bin', 'rb+')
+
+    context = file.readlines()
+    file.close()
+
+    existstate = False
+    for line in context:
+        currstate = line.strip().split(',')[1]
+        if state == currstate:
+            existstate = True
+            break
+
+    if not existstate:
+        print "fail state did not exist in add poll"
+        return "fail"
+
+    #check poll id
+    if filetype == 't':
+        file = open('Polls.txt', 'a+')
+    else:
+        file = open('copypolls.bin', 'ab+')
+    context = file.readlines()
+    for line in context:
+        line = line.split(',')[0]
+        if pid == line:
+            print "fail poll already exist"
+            return "fail"
+    file.write("%s,%s,%s,%s\n" % (pid, party, state, res))
+    file.close()
     return "fail"
 
 
@@ -137,8 +190,26 @@ for line in con:
 #addState('IS', 'ISRAEL', 'b')
 #addPoll(12,'asd','ALL','ewq','t')
 
+'''
 line = "TB1,Republican,AR,Ted Cruz 27%-Marco Rubio 23%-Donald Trump 23%-Ben Carson 11%"
 line = line.strip()
-split = line.split('%')
-for i in split:
-    print i.split(' ')[-1]
+print line
+split = line.split(',')[3]
+print split
+split = split.replace('-', '')
+split = split.split('%')
+split = split[:-1]
+print "-----------------"
+print split
+i=0
+for a in split:
+    a = str(a)
+    a = a.split(' ')[:2]
+    split[i]=a[0]+","+a[1]+",Democratic"
+    i=i+1
+    print a
+print split
+split = split[1:]
+print split
+'''
+addPoll("SH2","Republican","IS","Ted Cruz 27%-Marco Rubio 23%-michael shvili 23%-Ben Carson 11%","t")
