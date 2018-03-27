a9ba3680b49c12d384b3e867553978a0f5336349
NAre5 <sherlord5@gmail.com>
2018-03-27 17:49:33 +0300

    finaloooooooo

--- ex1.py
+++ ex1.py
@@ -1,4 +1,5 @@
-import numpy as np
+import csv,operator
+
 def File_Type(filetype):
     dict = {'t':'txt','b':'bin'}
     if dict.has_key(filetype):
@@ -18,21 +19,39 @@ def printid ():
 def addCandidate(fname, lname, party, filetype):
     return "fail"
 
-def delete_from(fileName,content,filetype):
-    File = open(fileName+File_Type(filetype), mode("r+",filetype))
+
+def delete_from(fileName, key, filetype):
+    """
+
+    :param fileName: the name of the file we want to delete from
+    :param key: the key of the record we want to delete
+    :param filetype: the type of the file we want to delete from
+    :return: if the deletion is valid and we succeeded to delete
+    """
+
+    File = open(fileName+'.'+File_Type(filetype), mode("r+",filetype))
     Lines = File.readlines()
-    #File.seek(0)
+    File.seek(0)
+    column = 0 + (fileName == 'States')
+    flag=False
     for line in Lines:
         splitted = line.strip().split(',')
-        print(splitted[0])
-        if str(content) == splitted[0]:
+        if str(key) == splitted[column]:
+            flag=True
             continue
         File.write(line)
     File.truncate()
     File.close()
-    return "success"
+    return "pass" if flag else 'fail'
 
 def deleteCandidate(cid,filetype):
+    """
+    The function delete a candidate from the candidates file if the deletion is valid
+    :param cid: the key of the record we want to delete
+    :param filetype: the type of file we want to delete from
+    :return: if the deletion is valid and we succeeded to delete
+    """
+
     File = open('Polls.' + File_Type(filetype), 'r+')
     arr = [[]]
     for line in File:
@@ -45,36 +64,40 @@ def deleteCandidate(cid,filetype):
         candidates[i] = candidates[i].split('%')[:-1]
         for j in range(candidates[i].__len__()):
             candidates[i][j] = candidates[i][j].split(' ')[:-1]
-            #candidates[i][j] = candidates[i][j][0] + ' ' + candidates[i][j][1]
 
     candidates = [candidates[i][j] for i in range(candidates.__len__()) for j in range(candidates[i].__len__())]
-    #print(candidates)
     File = open('Candidates.' + File_Type(filetype), 'r+')
     all_candidates = [[]]
     for line in File:
         all_candidates.append(line.strip().split(',')[0:3])
     all_candidates = all_candidates[2:]
     File.close()
-    #all_candidates.remove(all_candidates[0])
-    #print(arr)
+
 
     for i in range(all_candidates.__len__()):
         if all_candidates[i][0]==str(cid):
             if all_candidates[i][1:] in candidates:
                 return 'fail'
-    return delete_from('Candidates.',cid,filetype)
+    return delete_from('Candidates',cid,filetype)
 
 def addState(sid, sname,filetype):
     return "fail"
 
 
 def deleteState(sid,filetype):
-    File = open('Polls'+File_Type(filetype), 'r+')
+    """
+        The function delete a state from the states file if the deletion is valid
+        :param sid: the key of the record we want to delete
+        :param filetype: the type of file we want to delete from
+        :return: if the deletion is valid and we succeeded to delete
+        """
+
+    File = open('Polls.'+File_Type(filetype), 'r+')
     arr = [[]]
     for line in File:
         arr.append(line.strip().split(','))
     arr.remove(arr[0])
-    states = [li[1] for li in (ar for ar in arr)]
+    states = [li[2] for li in (ar for ar in arr)]
     File.close()
     if str(sid) in states:
         return 'fail'
@@ -164,6 +187,12 @@ def addPoll(pid, party, state, res, filetype):
 
 
 def deletePoll(pid,filetype):
+    """
+    The function delete a poll from the polls file if the deletion is valid
+    :param cid: the key of the record we want to delete
+    :param filetype: the type of file we want to delete from
+    :return: if the deletion is valid and we succeeded to delete
+    """
     return delete_from('Polls',pid,filetype)
 
 
@@ -173,7 +202,27 @@ def updateFile(recordId,fieldname,newValue,file,filetype):
 
 
 def sort(filename,filetype,fieldname):
-    return "fail"
+    File = open(filename + '.' + File_Type(filetype), mode("r+", filetype))
+    Title = File.readline()
+    splittedTitle = Title.strip().split(',')
+    dict = {}
+    for i in range(splittedTitle.__len__()):
+        dict[splittedTitle[i]] = i
+    if not dict.has_key(fieldname):
+        File.close()
+        return 'fail'
+    File.seek(0)
+    File.seek(File.readline().__len__() + (filetype=='t'))
+    lines = [line.split(',') for line in File]
+    File.close()
+    File = open(filename + '.' + File_Type(filetype), mode("w", filetype))
+    File.write(Title)
+
+    SOSO = sorted(lines, key=operator.itemgetter(dict[fieldname]))
+    for line in SOSO:
+        File.write(','.join(line))
+    File.close()
+    return "pass"
 
 
 def selectPollsFromStateAndParty(state,party,filetype):
@@ -185,9 +234,18 @@ def selectPollsForParty(party,filetype):
     l = []
     return l
 
+
 def find(filename,filetype,findme):
-    File = open(filename+File_Type(filetype), mode("r+",filetype))
+    """
+
+    :param filename: the name of the file we want to search in
+    :param filetype: the type of the file we want to search in
+    :param findme: the content we search in the file
+    :return: the number of the line the variable 'findme' appears
+    """
+    File = open(filename+'.'+File_Type(filetype), mode("r+",filetype))
     Lines = File.readlines()
+    File.close()
     for i in range(Lines.__len__()):
         if str(findme) in Lines[i]:
             return i
@@ -195,21 +253,71 @@ def find(filename,filetype,findme):
 
 
 def returnLine(filename,filetype,linenumber):
+    """
+
+    :param filename: the name of the file we want to search in
+    :param filetype: the type of the file we want to search in
+    :param linenumber: the number of the line we want to have its key
+    if the number is negative the number of the line will be counted from the end
+    :return: the key of the requested line
+    """
+
     File = open(filename+ '.' + File_Type(filetype), mode("r+", filetype))
     Lines = File.readlines()
+    File.close()
+    if int(linenumber) >= Lines.__len__():
+        return "fail"
     column = 0+ (filename=='States')
-    row = linenumber if linenumber>=0 else linenumber+Lines.__len__()
+    row = int(linenumber) if linenumber>=0 else int(linenumber)+Lines.__len__()
     return str(Lines[row]).strip().split(',')[column]
 
-print(returnLine('Polls','t',-1))
 
-#deleteCandidate(104,'t')
+print sort('Polls','t','party')
+
+
+
+
+
 '''
-File = open('Candidates.txt', 'r+')
-arr=[[]]
-for line in File:
-     arr.append(line.strip().split(','))
-arr.remove(arr[0])
-names = [li[1] for li in (ar for ar in arr)]
+import csv,operator
+
+#FileToWrite = open('newFile','w+')
+File = open('can.'+File_Type('t'), mode("r+",'b'))
+Title=File.readline()
+File.seek(0)
+#FileToWrite.write(Title)
+File.seek(File.readline().__len__())
+data = csv.reader(File,delimiter=',')
+sorted = sorted(data,key=operator.itemgetter(0))
+
+print sorted[0]
+File = open('can.'+File_Type('t'), mode("w+",'t'))
+print sorted[0]
+fileWriter = csv.writer(File,delimiter=',')
+fileWriter.writerow(sorted[0])
+'''
+
+
+
+
+
+'''
+#File.seek(File.readline().__len__()+1)
+print File.readlines()
+fileWriter = csv.writer(File,delimiter=',')
+
+File.close()
+File = open('can.'+File_Type('t'), mode("r+",'t'))
+#File.seek(File.readline().__len__()+1)
+File.seek(0)
+#FileToWrite = open('newFile','r+')
+after = File.readlines()
+#print after
+
+for i in range(after.__len__()):
+    File.write(after[i].strip())
+print after
+
+
 File.close()
 '''
